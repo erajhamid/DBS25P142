ae88e2439a438b1c5309bbf1b59f69d1fe726b2d
erajhamid <erajhamid4@gmail.com>
2025-03-01 04:33:41 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MidProjectDb
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelSideBar = new Panel();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel1 = new Panel();
+            panelSideBar.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelSideBar
+            // 
+            panelSideBar.BackColor = SystemColors.Menu;
+            panelSideBar.Controls.Add(button4);
+            panelSideBar.Controls.Add(button3);
+            panelSideBar.Controls.Add(button2);
+            panelSideBar.Controls.Add(button1);
+            panelSideBar.Dock = DockStyle.Left;
+            panelSideBar.Location = new Point(0, 0);
+            panelSideBar.Margin = new Padding(4);
+            panelSideBar.Name = "panelSideBar";
+            panelSideBar.Size = new Size(263, 641);
+            panelSideBar.TabIndex = 0;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.MenuHighlight;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(28, 570);
+            button4.Margin = new Padding(4);
+            button4.Name = "button4";
+            button4.Size = new Size(196, 74);
+            button4.TabIndex = 3;
+            button4.Text = "Finance";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.MenuHighlight;
+            button3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(28, 400);
+            button3.Margin = new Padding(4);
+            button3.Name = "button3";
+            button3.Size = new Size(196, 82);
+            button3.TabIndex = 2;
+            button3.Text = "Participants";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.MenuHighlight;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(28, 238);
+            button2.Margin = new Padding(4);
+            button2.Name = "button2";
+            button2.Size = new Size(196, 73);
+            button2.TabIndex = 1;
+            button2.Text = "Events";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.MenuHighlight;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(28, 69);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(196, 73);
+            button1.TabIndex = 0;
+            button1.Text = "Dashboard";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Menu;
+            panel1.Dock = DockStyle.Top;
+            panel1.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            panel1.Location = new Point(263, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(918, 194);
+            panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveCaption;
+            ClientSize = new Size(1181, 641);
+            Controls.Add(panel1);
+            Controls.Add(panelSideBar);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.InactiveCaption;
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panelSideBar.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelSideBar;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Panel panel1;
+    }
+}
