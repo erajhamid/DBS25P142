02ffca87cfb57580b5d8852f29d2cae3a4e2a746
erajhamid <erajhamid4@gmail.com>
2025-03-20 12:03:01 -0400

    Add files via upload

---
+++ facultydashboard.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MidProjectDb
+{
+    partial class facultydashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(facultydashboard));
+            label2 = new Label();
+            panel1 = new Panel();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(393, 39);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(486, 81);
+            label2.TabIndex = 1;
+            label2.Text = "FACULTY DASHBOARD";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(1, 0);
+            panel1.Margin = new Padding(4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(284, 633);
+            panel1.TabIndex = 2;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ButtonShadow;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(32, 471);
+            button4.Name = "button4";
+            button4.Size = new Size(212, 62);
+            button4.TabIndex = 3;
+            button4.Text = "Assign Duties";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonShadow;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(32, 329);
+            button3.Name = "button3";
+            button3.Size = new Size(212, 65);
+            button3.TabIndex = 2;
+            button3.Text = "Check Results";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ButtonShadow;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(32, 184);
+            button2.Name = "button2";
+            button2.Size = new Size(212, 60);
+            button2.TabIndex = 1;
+            button2.Text = "View Participants";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonShadow;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(32, 49);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(212, 58);
+            button1.TabIndex = 0;
+            button1.Text = "Manage Events";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // facultydashboard
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1029, 630);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ControlLightLight;
+            Margin = new Padding(4);
+            Name = "facultydashboard";
+            Text = "facultydashboard";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Label label2;
+        private Panel panel1;
+        private Button button2;
+        private Button button1;
+        private Button button4;
+        private Button button3;
+    }
+}
---
+++ itec.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MidProjectDb
+{
+    partial class itec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(itec));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(1, 1);
+            label1.Name = "label1";
+            label1.Size = new Size(491, 119);
+            label1.TabIndex = 0;
+            label1.Text = "ITEC MANAGEMENT";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(130, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 33);
+            label2.TabIndex = 1;
+            label2.Text = "Itec_ID";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(278, 200);
+            textBox1.MaxLength = 5;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Enter itec id";
+            textBox1.Size = new Size(272, 27);
+            textBox1.TabIndex = 2;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(130, 242);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 31);
+            label3.TabIndex = 3;
+            label3.Text = "Itec Year";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(278, 286);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Enter year";
+            textBox2.Size = new Size(272, 27);
+            textBox2.TabIndex = 4;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            label4.BackColor = SystemColors.ActiveCaptionText;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(130, 331);
+            label4.Name = "label4";
+            label4.Size = new Size(150, 29);
+            label4.TabIndex = 5;
+            label4.Text = "Theme";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(280, 375);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Theme of Itec";
+            textBox3.Size = new Size(275, 27);
+            textBox3.TabIndex = 6;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            label5.BackColor = SystemColors.ActiveCaptionText;
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.FlatStyle = FlatStyle.Flat;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(130, 418);
+            label5.Name = "label5";
+            label5.Size = new Size(150, 31);
+            label5.TabIndex = 7;
+            label5.Text = "Description";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(280, 465);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Any description";
+            textBox4.Size = new Size(277, 25);
+            textBox4.TabIndex = 8;
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonShadow;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(582, 491);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 43);
+            button1.TabIndex = 9;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // itec
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 549);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "itec";
+            Text = "itec";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private Button button1;
+    }
+}
