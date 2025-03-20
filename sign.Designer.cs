cc3489f0b9e40483c735d8731b8843fa0c595e88
erajhamid <erajhamid4@gmail.com>
2025-03-20 12:00:25 -0400

    Add files via upload

---
+++ sign.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace MidProjectDb
+{
+    partial class sign
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sign));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(1, -1);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(667, 149);
+            label1.TabIndex = 0;
+            label1.Text = "ITEC MANAGEMENT";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(189, 193);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(194, 41);
+            label2.TabIndex = 1;
+            label2.Text = "Username";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(356, 252);
+            textBox1.Margin = new Padding(5);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Enter your username";
+            textBox1.Size = new Size(390, 29);
+            textBox1.TabIndex = 2;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(189, 307);
+            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(194, 41);
+            label3.TabIndex = 3;
+            label3.Text = "Email";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(356, 370);
+            textBox2.Margin = new Padding(5);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Enter your email";
+            textBox2.Size = new Size(390, 29);
+            textBox2.TabIndex = 4;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            label5.BackColor = SystemColors.ActiveCaptionText;
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.FlatStyle = FlatStyle.Flat;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(189, 421);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(194, 40);
+            label5.TabIndex = 6;
+            label5.Text = "Password";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(356, 486);
+            textBox3.Margin = new Padding(4);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Enter password";
+            textBox3.Size = new Size(390, 29);
+            textBox3.TabIndex = 7;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonShadow;
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(726, 584);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 40);
+            button1.TabIndex = 11;
+            button1.Text = "Sign in";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += Button1_Click;
+            // 
+            // sign
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1258, 749);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Cursor = Cursors.Hand;
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5);
+            Name = "sign";
+            Text = "sign";
+            Load += sign_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
---
+++ sign.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+
+namespace MidProjectDb
+{
+    public partial class sign : Form
+    {
+
+
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password_hash { get; set; }
+       
+
+       
+        public int user_id { get; set; }
+
+        public sign()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            username = textBox1.Text;
+            email = textBox2.Text;
+            password_hash = textBox3.Text;
+           
+
+            
+            
+
+            try
+            {
+                using (MySqlConnection conn = DatabaseHelper.Instance.getConnection())
+                {
+                    conn.Open(); // 
+
+                  
+
+
+                    // SQL query
+                    string query = "INSERT INTO users ( user_id,username, email, password_hash) VALUES (@user_id, @Username, @Email, @Password)";
+
+                    using (MySqlCommand cmd = new(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@user_id", user_id);
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Email", email);
+                        cmd.Parameters.AddWithValue("@Password", password_hash);
+                        
+
+
+                        cmd.ExecuteNonQuery();
+                    }
+                   
+
+                    MessageBox.Show("User registered successfully!");
+                    this.Hide();
+                    facultydashboard facul = new();
+                    facul.Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void sign_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+       
+
