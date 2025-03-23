ba933b29b895eeedc801b0032924073369ec53b1
erajhamid <erajhamid4@gmail.com>
2025-03-23 07:04:13 -0400

    Add files via upload

---
+++ venue.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace MidProjectDb
+{
+    partial class venue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(venue));
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Dock = DockStyle.Top;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(862, 101);
+            label1.TabIndex = 0;
+            label1.Text = "Venue Allocations";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(178, 135);
+            label2.Name = "label2";
+            label2.Size = new Size(164, 35);
+            label2.TabIndex = 1;
+            label2.Text = "Event Name";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(339, 182);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(265, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(178, 220);
+            label3.Name = "label3";
+            label3.Size = new Size(164, 35);
+            label3.TabIndex = 3;
+            label3.Text = "Venue Name";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(339, 271);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(265, 23);
+            textBox1.TabIndex = 4;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            label4.BackColor = SystemColors.ActiveCaptionText;
+            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.FlatStyle = FlatStyle.Flat;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(178, 305);
+            label4.Name = "label4";
+            label4.Size = new Size(164, 36);
+            label4.TabIndex = 5;
+            label4.Text = "Capacity";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(339, 355);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Maximum capacity";
+            textBox2.Size = new Size(267, 27);
+            textBox2.TabIndex = 6;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            label5.BackColor = SystemColors.ActiveCaptionText;
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.FlatStyle = FlatStyle.Flat;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(178, 394);
+            label5.Name = "label5";
+            label5.Size = new Size(164, 31);
+            label5.TabIndex = 7;
+            label5.Text = "Location";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(339, 438);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Location for Event";
+            textBox3.Size = new Size(268, 27);
+            textBox3.TabIndex = 8;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            label6.BackColor = SystemColors.ActiveCaptionText;
+            label6.BorderStyle = BorderStyle.FixedSingle;
+            label6.FlatStyle = FlatStyle.Flat;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(178, 478);
+            label6.Name = "label6";
+            label6.Size = new Size(164, 33);
+            label6.TabIndex = 9;
+            label6.Text = "Assigned Date";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(339, 524);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(265, 23);
+            dateTimePicker1.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.BackColor = SystemColors.ActiveCaptionText;
+            label7.BorderStyle = BorderStyle.FixedSingle;
+            label7.FlatStyle = FlatStyle.Flat;
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ControlLightLight;
+            label7.Location = new Point(178, 566);
+            label7.Name = "label7";
+            label7.Size = new Size(164, 34);
+            label7.TabIndex = 11;
+            label7.Text = "Assigned Time";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(339, 615);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(273, 23);
+            textBox4.TabIndex = 12;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonShadow;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(651, 654);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 41);
+            button1.TabIndex = 13;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // venue
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(862, 721);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(label7);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "venue";
+            Text = "venue";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
+        private Label label7;
+        private TextBox textBox4;
+        private Button button1;
+    }
+}
